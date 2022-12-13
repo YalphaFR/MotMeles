6a32aced97cb9564bdb51fe630242bb5b76ed65e
Alexandre Nguyen <alexandrethanhcao@free.fr>
2022-12-13 10:52:10 +0100

    Commentaires sur Plateaux et Joueurs finis, ToFile et ToReadFile dans Joueur créé

--- Joueur.cs
+++ Joueur.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,7 @@ namespace MotMeles_v1
     internal class Joueur
     {
         private string nom;
-        private List<string> mots; //possibilité erreur avec liste
+        private List<string> mots; 
         private int score;
         public Joueur(string nom, List<string> mots = null, int score = 0)
         {
@@ -20,33 +21,31 @@ namespace MotMeles_v1
 
         public string Nom
         {
-            get { return nom; }
-            set { nom = value; }
+            get { return this.nom; }
         }
 
-        public List<string> Mots //possibilité erreur avec liste
+        public List<string> Mots
         {
-            get { return mots; }
-            set { mots = value; }
+            get { return this.mots; }
+            set { this.mots = value; }
         }
 
         public int Score
         {
-            get { return score; }
-            set { score = value; }
+            get { return this.score; }
         }
         /// <summary>
         /// Add_Mot rajoute un mot à la liste mots du Joueur
         /// </summary>
-        /// <param name="mot"></param>
-        public void Add_Mot(string mot) //possibilité erreur avec liste
+        /// <param name="mot">le mot à ajouter</param>
+        public void Add_Mot(string mot) 
         {
             this.mots.Add(mot);
         }
         /// <summary>
         /// Renvoie un string décrivant le Joueur
         /// </summary>
-        /// <returns></returns>
+        /// <returns>Renvoie un string décrivant le Joueur</returns>
         public override string ToString()
         {
             string resultat = nom + " " + score + " points" + "/n" + "Mots trouvés : /n";
@@ -56,5 +55,55 @@ namespace MotMeles_v1
             }
             return resultat;
         }
+        /// <summary>
+        /// Enregistre un Joueur dans un fichier csv
+        /// </summary>
+        /// <param name="nomfile">chemin du fichier</param>
+        public void ToFile(string nomfile)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter(nomfile);
+                string ligne = "";
+                sw.WriteLine(this.nom + ";");
+                sw.WriteLine("Score : "+";"+this.score);
+                ligne = string.Join(";", this.mots);
+                sw.WriteLine(ligne);
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        /// <summary>
+        /// Permet d'associer des valeurs à un Joueur à partir d'un csv
+        /// </summary>
+        /// <param name="nomfile">chemin du fichier</param>
+        public void ToReadFile(string nomfile)
+        {
+            try
+            {
+                StreamReader sr = new StreamReader(nomfile);
+                string ligne = "";
+                ligne = sr.ReadLine();
+                string[] l = ligne.Split(';');
+                this.nom = l[0];
+                ligne = sr.ReadLine();
+                l = ligne.Split(';');
+                this.score = int.Parse(l[1]);
+                ligne=sr.ReadLine();
+                string[] mots= ligne.Split(';');
+                for(int i = 0; i < mots.Length; i++)
+                {
+                    this.mots.Add(mots[i]);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
     }
+    
 }
